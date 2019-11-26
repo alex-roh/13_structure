f328489a8f24c66782cd7d204cc7e455224a884f
eunyoungroh <roheunyoung@outlook.com>
2019-11-26 15:32:27 +0900

    실습 2

--- 13_structure/Source.c
+++ 13_structure/Source.c
@@ -1,5 +1,6 @@
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <string.h>
 
 struct student {
 	
@@ -12,6 +13,13 @@ struct student {
 int main() {
 
 	struct student s1 = { 24, "eunyoung roh", 4.3 };
+	s1.id = 1234;
+	strcpy(s1.name, "another name");
+	s1.grade = 1.0;
+
+	printf("id: %d\n", s1.id);
+	printf("name: %s\n", s1.name);
+	printf("grade: %f\n", s1.grade);
 
 	return 0;
 }
